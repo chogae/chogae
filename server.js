498471949a6bc49d625ce98157fafc3a7db7c71e
전범준 <gagl142857@gmail.com>
2025-12-01 21:34:46 +0900

    update

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
-ECHO가 설정되어 있습니다.
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+app.use(express.static(path.join(__dirname)));
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "index.html"));
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT);
